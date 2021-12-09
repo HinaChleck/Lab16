982df631a7a342907744c8f4576ea56123322b6a
HinaChleck <hinachleck@gmail.com>
2021-12-09 21:19:44 +0500

    Обновлено

--- 2Deserialize/Program.cs
+++ 2Deserialize/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Json.Serialization;
 
-namespace _2Deserialize
+
+namespace Lab16
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Product[] products = new Product[5];
+            string path = "C:\\Users\\v.baranova\\Documents\\Автоматизация BIM\\Labs\\Lab16\\Lab16\\bin\\Debug\\Products.json";
+            if (!File.Exists(path)) Console.WriteLine("Файл не найден");
+            else
+
+        }
+
+
+        public class Product
+        {
+            [JsonPropertyName("Код товара")]
+            public uint Code { get; set; }
+            [JsonPropertyName("Название товара")]
+            public string Name { get; set; }
+            [JsonPropertyName("Цена товара")]
+            public decimal Price { get; set; }
         }
     }
 }
