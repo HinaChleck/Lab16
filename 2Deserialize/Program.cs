4219fc8229718475953556956e18946ed97ec558
HinaChleck <hinachleck@gmail.com>
2021-12-10 19:00:47 +0500

    Доделано

--- 2Deserialize/Program.cs
+++ 2Deserialize/Program.cs
@@ -8,18 +8,61 @@ using System.IO;
 using System.Text.Json.Serialization;
 
 
+
+
 namespace Lab16
 
 {
     internal class Program
     {
         static void Main(string[] args)
+
+        /*2.    Необходимо разработать программу для получения информации о товаре из json-файла.
+        Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.*/
+
         {
-            Product[] products = new Product[5];
             string path = "C:\\Users\\v.baranova\\Documents\\Автоматизация BIM\\Labs\\Lab16\\Lab16\\bin\\Debug\\Products.json";
             if (!File.Exists(path)) Console.WriteLine("Файл не найден");
             else
+            {
+                string line;
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    line = sr.ReadToEnd();
+                }
 
+                string[] readProducts = line.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Product[] products = new Product[5];
+
+                for (int i = 0; i < products.Length; i++)
+                {
+                    products[i] = JsonSerializer.Deserialize<Product>(readProducts[i]);
+                    Console.Write("#{0,10}", products[i].Code);
+                    Console.Write(": {0,25} ", products[i].Name);
+                    Console.Write("Цена: {0:f2}\n", products[i].Price);
+                }
+
+                decimal maxPrice = 0;
+                for (int i = 0; i < products.Length; i++)
+                {
+                    if (products[i].Price > maxPrice)
+                    {
+                        maxPrice = products[i].Price;
+                    }
+                }
+
+                Console.Write("\nСамые дорогие продукты: ");
+                for (int i = 0; i < products.Length; i++)
+                {
+                    if (products[i].Price == maxPrice)
+                    {
+                        Console.Write("{0}, ", products[i].Name);
+                    }
+                }
+
+            }
+            Console.ReadKey();
         }
 
 
--- Lab16/Program.cs
+++ Lab16/Program.cs
@@ -7,7 +7,7 @@ using System.Text.Json;
 using System.IO;
 using System.Text.Json.Serialization;
 
-namespace Lab16
+namespace Serialize
 {
     internal class Program
     {
@@ -19,47 +19,6 @@ namespace Lab16
         Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json».*/
         static void Main(string[] args)
         {
-            #region Задача 8
-            /*{
-
-            string path = @"Lab8_2.txt";
-            if (File.Exists(path))   File.Delete(path);
-
-            #region Создание и запись в файл массива случайных чисел
-            string[] row=new string[10];
-            Random random = new Random();
-
-            for (int i = 0; i < 10; i++)
-            {
-                row[i] = Convert.ToString(random.Next(0, 10));
-                using (StreamWriter sw = new StreamWriter(path, true)) 
-                { 
-                    sw.Write("{0} ",row[i]); 
-                }                
-            }
-            #endregion
-            string line;
-            using (StreamReader sr=new StreamReader(path))            
-            {
-                line =sr.ReadLine();
-            }
-
-            line= line.Trim();
-            string[] readrow = line.Split();
-
-            int sum = 0;
-            for (int i = 0; i < readrow.Length; i++)
-            {
-                int number=Convert.ToInt32(readrow[i]);
-                sum+=number;
-            }
-            Console.WriteLine("Сумма чисел в файле = {0}",sum);
-            Console.WriteLine("\n(Создан файл: {0})", Path.GetFullPath(path));
-            Console.ReadKey();  
-        }*/
-            #endregion
-
-            
             Product[] products = new Product[5];
             string path = "C:\\Users\\v.baranova\\Documents\\Автоматизация BIM\\Labs\\Lab16\\Lab16\\bin\\Debug\\Products.json";
             if (File.Exists(path)) File.Delete(path);
@@ -68,56 +27,48 @@ namespace Lab16
                 for (int i = 0; i < products.Length; i++)
                 {
                     products[i] = new Product();
-
-
                     uint code;
-                        Console.Write("Введите код товара №{0}: ", i + 1);
-                        string codeS = Console.ReadLine();
+                    Console.Write("Введите код товара №{0}: ", i + 1);
+                    string codeS = Console.ReadLine();
                     if (UInt32.TryParse(codeS, out code))
                     {
                         products[i].Code = code;
                         sw.Write("{");
-                        sw.Write("\"Код товара\":\"{0}\",", code);
+                        sw.Write("\"Код товара\":{0},", code);
                     }
                     else
                     {
                         products[i].Code = 0;
                         sw.Write("{");
-                        sw.Write("\"Код товара\":\"{0}\",", 0);
+                        sw.Write("\"Код товара\":{0},", 0);
                         Console.WriteLine("Предупреждение: неверный формат введенных данных. Код товара не записан");
-                    }                    
-                    
+                    }
+
                     Console.Write("Введите наименование товара №{0}: ", i + 1);
                     products[i].Name = Console.ReadLine();
                     sw.Write("\"Название товара\":\"{0}\",", products[i].Name);
-                   
+
                     decimal price;
                     Console.Write("Введите цену товара № {0}: ", i + 1);
-                        string priceS = Console.ReadLine();
+                    string priceS = Console.ReadLine();
                     if (Decimal.TryParse(priceS, out price))
                     {
 
                         products[i].Price = price;
-                        sw.Write("\"Цена товара\":\"{0}\"", products[i].Price);
-                        sw.Write("}");
+                        sw.Write("\"Цена товара\":{0}", products[i].Price);
+                        sw.Write("}\n");
                     }
                     else
                     {
                         products[i].Price = price;
-                        sw.Write("\"Цена товара\":\"{0}\"", 0);
-                        sw.Write("}");
+                        sw.Write("\"Цена товара\":{0}", 0);
+                        sw.Write("}\n");
                         Console.WriteLine("Предупреждение: неверный формат введенных данных. Цена товара не записана");
                     }
-                    
+
                 }
             }
-            /*for (int i = 0; i < products.Length; i++)
-            {
-                Console.WriteLine(products[i].Code);
-                Console.WriteLine(products[i].Name);
-                Console.WriteLine(products[i].Price); 
-            }
-            */
+
             Console.ReadKey();
         }
 
